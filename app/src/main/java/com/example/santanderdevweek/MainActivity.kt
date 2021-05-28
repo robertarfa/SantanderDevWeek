b5b44853e87bd7f086752543d919dd82188aedd8
Roberta Aguilar <roberta.aguilar@kobel.com.br>
2021-05-28 00:00:13 -0300

    style: starting layout

--- app/src/main/java/com/example/santanderdevweek/MainActivity.kt
+++ app/src/main/java/com/example/santanderdevweek/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return true
+        //sempre seja exibido
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_1 -> {
+                Log.d("Click","Click item 1")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+
+        }
     }
 }
